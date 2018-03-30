0f0b9f3f90e4b5016aaf58ffd610c59e4ba81c17
Ryo.Kayama <rr_poly_1212@yahoo.co.jp>
2018-03-30 21:51:26 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ryo/musicsample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ryo.musicsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
